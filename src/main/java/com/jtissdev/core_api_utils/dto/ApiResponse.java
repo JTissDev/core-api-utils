b25d43889e701711d6bcd0fadad7a236fb7a52d7
J.Tissier <jtdevweb@gmail.com>
2025-05-11 18:28:16 +0200

    ApiResponse

--- src/main/java/com/jtissdev/core_api_utils/dto/ApiResponse.java
+++ src/main/java/com/jtissdev/core_api_utils/dto/ApiResponse.java
@@ -21,5 +21,27 @@ public class ApiResponse<T> {
 		return new ApiResponse<>(false, null, message);
 	}
 
-	// Getters & setters omitted pour concision
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
