94c28cfa045e2cec7f2e0fc567aa65f102adfaf4
J.Tissier <jtdevweb@gmail.com>
2025-05-11 18:28:37 +0200

    ApiUtils

---
+++ src/main/java/com/jtissdev/core_api_utils/ApiUtils.java
@@ -0,0 +1,9 @@
+package com.jtissdev.core_api_utils;
+
+public class ApiUtils {
+	public static String VERSION = "1.0.0";
+
+	public static void init() {
+		System.out.println("api-utils loaded. Version: " + VERSION);
+	}
+}
