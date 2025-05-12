6fb9e554c5563866409a46ab6233ed01c36a1b41
J.Tissier <jtdevweb@gmail.com>
2025-05-12 14:14:27 +0200

    Info and help request

--- src/main/java/com/jtissdev/core_api_utils/requestgen/ApiHelpStructure.java
+++ src/main/java/com/jtissdev/core_api_utils/requestgen/ApiHelpStructure.java
@@ -1,4 +1,22 @@
 package com.jtissdev.core_api_utils.requestgen;
 
+import java.util.Map;
+
 public class ApiHelpStructure {
+
+	public static Map<String, Object> standardHeaders() {
+		return Map.of(
+				"X-Help-Request", true,
+				"X-Request-Type", "HELP",
+				"Accept", "application/json"
+		);
+	}
+
+	public static Map<String, Object> standardResponseBody(String endpoint, String description, Map<String, Object> exampleRequest) {
+		return Map.of(
+				"endpoint", endpoint,
+				"description", description,
+				"request", exampleRequest
+		);
+	}
 }
--- src/main/java/com/jtissdev/core_api_utils/requestgen/ApiInfoBuilder.java
+++ src/main/java/com/jtissdev/core_api_utils/requestgen/ApiInfoBuilder.java
@@ -1,4 +1,15 @@
 package com.jtissdev.core_api_utils.requestgen;
 
+import java.time.Instant;
+import java.util.Map;
+
 public class ApiInfoBuilder {
+
+	public static Map<String, Object> buildInfo(String serviceName, String version) {
+		return Map.of(
+				"service", serviceName,
+				"version", version,
+				"timestamp", Instant.now().toString()
+		);
+	}
 }
